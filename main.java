857113c945a2e63c78b82bfa707d166bd65fd7f5
Deekshith1996 <103950541+Deekshith1996@users.noreply.github.com>
2022-09-18 17:30:04 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.Lockedme;
+
+
+public class main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "E:\\deekshie"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
